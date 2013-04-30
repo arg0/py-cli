d9f7841ccbad4fcb20f30c068dfb0a6dcbe26b5e
Leif Johnson <leif@leifjohnson.net>
2013-04-30 12:31:06 -0500

    First checkin.

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+import setuptools
+
+setuptools.setup(
+    name='lmj.cli',
+    version='0.0.1',
+    namespace_packages=['lmj'],
+    packages=setuptools.find_packages(),
+    author='Leif Johnson',
+    author_email='leif@leifjohnson.net',
+    description='Command-line utilities',
+    long_description=open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'README.md')).read(),
+    license='MIT',
+    url='http://github.com/lmjohns3/py-cli/',
+    keywords=('command-line '
+              'logging '
+              'arguments '
+              ),
+    install_requires=['plac'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        ],
+    )
