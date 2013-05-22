8e2d3f11a36c41fe27489254d938f9b3d0ba3172
Leif Johnson <leif@leifjohnson.net>
2013-05-22 12:47:29 -0500

    Bump version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name='lmj.cli',
-    version='0.0.1',
+    version='0.0.2',
     namespace_packages=['lmj'],
     packages=setuptools.find_packages(),
     author='Leif Johnson',
